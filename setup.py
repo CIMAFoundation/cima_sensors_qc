887f634f4ca4f2eb0f2205969cf5ba5bdc367cf6
Perello-nico <perellonicolo@gmail.com>
2022-12-27 12:02:24 +0100

    last comments; update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="risicolive_QC",
-    version="0.1.1",
+    version="0.2.0",
     author="Nicolò Perello",
     author_email="nicolo.perello@cimafoundation.org",
     description="data quality check algorithm for RISICO Live stations",
