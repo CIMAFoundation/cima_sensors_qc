5d570c5c7df0e92ab47ed16de5792ecbf944b167
Perello-nico <perellonicolo@gmail.com>
2022-12-27 11:41:17 +0100

    modified comments

--- risicolive_QC/lib.py
+++ risicolive_QC/lib.py
@@ -8,21 +8,19 @@ class QualityLabel(Enum):
     Enum for the quality label
     """
     INCOMPLETE = 3
-    WRONG = 2
+    WRONG      = 2
     SUSPICIOUS = 1
-    GOOD = 0
+    GOOD       = 0
 
 
 ################################################################################
 def quality_label(qc_val):
     """
     This function assigns the quality label:
-    - good: all tests are passed
-    - suspicious: step or time persistence tests non passed
-    - wrong: range test non passed
-    - incomplete: complete or consistency tests not passed
-
-    Keyword arguments:
+    - incomplete: complete or consistency tests are not passed
+    - wrong:      range test is non passed
+    - suspicious: step or time persistence tests are non passed
+    - good:       all tests are passed
     qc_val -- value to check
     """
     if not((qc_val & FLAGS.OK_COMPLETE.value) and (qc_val & FLAGS.OK_CONSISTENT.value)):
@@ -40,16 +38,12 @@ def quality_label(qc_val):
 ################################################################################
 def quality_check(df_station: pd.DataFrame, settings: Dict=DEFAULT):
     """
-    This function compute all the tests sequentially for the single station
-
-    Keyword arguments:
+    This function compute the tests for the single station and add the label
     df_station -- pandas.dataframe with data for a single station [rows:times, columns:variables]
     settings   -- dictionary with config info for all tests
     """
     df_check = pd.DataFrame(index=df_station.index, columns=['QC', 'QC_LABEL'])
-
     internal_check = InternalCheck(settings)
     df_check.loc[:, 'QC'] = internal_check.all_test(df_station)
     df_check.loc[:, 'QC_LABEL'] = df_check['QC'].apply(lambda qc_val: quality_label(qc_val))
-
     return df_check
