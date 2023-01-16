dad39383b5bd9ddb756d2d312c9371ea73f64dfc
Perello-nico <perellonicolo@gmail.com>
2023-01-16 14:44:49 +0100

    setted all data are consistent; modified function order

--- risicolive_QC/tests.py
+++ risicolive_QC/tests.py
@@ -11,7 +11,7 @@ class FLAGS(Enum):
     """
     ALL_NO            = np.uint16(0b0000000000000000) # none of the tests are passed
     OK_COMPLETE       = np.uint16(0b0000000000000001) # complete test is passed
-    #OK_CONSISTENT     = np.uint16(0b0000000000000010) # consistent test is passed
+    OK_CONSISTENT     = np.uint16(0b0000000000000010) # consistent test is passed
     OK_RANGE          = np.uint16(0b0000000000000100) # range test is passed
     OK_NO_STEPS       = np.uint16(0b0000000000001000) # step test is passed
     OK_NO_PERSISTENCE = np.uint16(0b0000000000010000) # persistence test is passed
@@ -35,7 +35,7 @@ class InternalCheck():
         df_check = pd.Series(index=df_station.index, name='internal_check', dtype='uint16')
         df_check.loc[:] = FLAGS.ALL_NO.value
         df_check.loc[self.complete_test(df_station)]    += FLAGS.OK_COMPLETE.value
-        #df_check.loc[self.consistency_test(df_station)] += FLAGS.OK_CONSISTENT.value
+        df_check.loc[:]                                 += FLAGS.OK_CONSISTENT.value ## ALL ARE CONSISTENT
         df_check.loc[self.range_test(df_station)]       += FLAGS.OK_RANGE.value
         df_check.loc[self.step_test(df_station)]        += FLAGS.OK_NO_STEPS.value
         df_check.loc[self.persistence_test(df_station)] += FLAGS.OK_NO_PERSISTENCE.value
@@ -51,16 +51,6 @@ class InternalCheck():
         df                          -- pandas.dataframe with data for a single station [rows:times, columns:variables]
         """
         return df[self.settings['VARS_CHECK']].notna().all(axis=1, bool_only=True)
-
-    #def consistency_test(self, df: pd.DataFrame) -> pd.Series:
-    #    """
-    #    The function checks data consistency for two variables A and B in each time instant:
-    #    when A is NaN, B must be zero or NaN; when A is not NaN, B must be not NaN
-    #    This check is done for each time step (e.g. for each row of the dataset)
-    #    self.settings['VARS_CHECK'] -- variables A-B to check (WITH THIS ORDER)
-    #    df                          -- pandas.dataframe with data for a single station [rows:times, columns:variables]
-    #    """
-    #    return df[self.settings['VARS_CONS']].apply(lambda row: self.consistency_check(row), axis=1)
 
     def range_test(self, df: pd.DataFrame) -> pd.Series:
         """
@@ -106,25 +96,27 @@ class InternalCheck():
         return df_test.all(axis=1)
 
 
-    #def complete_check(self, array: np.array) -> bool:
-    #    """This function returns True if the array does NOT contain NaN values"""
-    #    return np.count_nonzero(np.isnan(array))==0
+    def range_check(self, vars_check: np.array, min_vals: np.array, max_vals: np.array) -> bool:
+        """This function returns True if element are in the range"""
+        return (vars_check>=min_vals) & (vars_check<=max_vals)
+
+    def no_persistence_check(self, var_check: np.array, min_variation: float, min_val: float, max_val: float) -> bool:
+        """This function returns True if elements are not constants"""
+        persistence = np.all((var_check>=min_val)&(var_check<=max_val)) and (np.abs(np.max(var_check)-np.min(var_check))<min_variation)
+        return not persistence
+
+    #def consistency_test(self, df: pd.DataFrame) -> pd.Series:
+    #    """
+    #    The function checks data consistency for two variables A and B in each time instant:
+    #    when A is NaN, B must be zero or NaN; when A is not NaN, B must be not NaN
+    #    This check is done for each time step (e.g. for each row of the dataset)
+    #    self.settings['VARS_CHECK'] -- variables A-B to check (WITH THIS ORDER)
+    #    df                          -- pandas.dataframe with data for a single station [rows:times, columns:variables]
+    #    """
+    #    return df[self.settings['VARS_CONS']].apply(lambda row: self.consistency_check(row), axis=1)
 
     #def consistency_check(self, array: np.array) -> bool:
     #    """This function returns True if the first element is zero or NaN when the second element is NaN"""
     #    a = array[0]
     #    b = array[1]
     #    return return ((not np.isnan(a)) & (not np.isnan(b))) or ( np.isnan(a) & ((b==0) | (np.isnan(b))))
-
-    def range_check(self, vars_check: np.array, min_vals: np.array, max_vals: np.array) -> bool:
-        """This function returns True if element are in the range"""
-        return (vars_check>=min_vals) & (vars_check<=max_vals)
-
-    #def no_step_check(self, array: np.array, step: float) -> bool:
-    #    """This function returns True if there are no step in a 2-d array"""
-    #    return (np.abs(array[1]-array[0])<=step)
-
-    def no_persistence_check(self, var_check: np.array, min_variation: float, min_val: float, max_val: float) -> bool:
-        """This function returns True if elements are not constants"""
-        persistence = np.all((var_check>=min_val)&(var_check<=max_val)) and (np.abs(np.max(var_check)-np.min(var_check))<min_variation)
-        return not persistence
