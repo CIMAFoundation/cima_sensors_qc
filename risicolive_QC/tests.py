f4c35539ee01b6c480453b4d4fb7d089189d47c5
Perello-nico <perellonicolo@gmail.com>
2023-01-16 10:22:50 +0100

    modified names of variables of functions

--- risicolive_QC/tests.py
+++ risicolive_QC/tests.py
@@ -72,7 +72,7 @@ class InternalCheck():
         variables = list(self.settings['RANGES'].keys())
         mins = [self.settings['RANGES'][v][0] for v in self.settings['RANGES'].keys()]
         maxs = [self.settings['RANGES'][v][1] for v in self.settings['RANGES'].keys()]
-        return df[variables].apply(lambda row: self.range_check(row, mins, maxs), axis=1).all(axis=1, bool_only=True)
+        return df[variables].apply(lambda vars_check: self.range_check(vars_check=vars_check, min_vals=mins, max_vals=maxs), axis=1).all(axis=1, bool_only=True)
 
     def step_test(self, df: pd.DataFrame) -> pd.Series:
         """
@@ -94,8 +94,11 @@ class InternalCheck():
         df                          -- pandas.dataframe with data for a single station [rows:times, columns:variables]
         """
         df_test = pd.DataFrame(index=df.index, columns=self.settings['VARIATIONS'].keys())
-        for vv in self.settings['VARIATIONS'].keys():
-            df_test.loc[:, vv] = df[vv].rolling(self.settings['WINDOW']).apply(lambda ww: self.no_persistence_check(ww, self.settings['VARIATIONS'][vv][0], self.settings['VARIATIONS'][vv][1], self.settings['VARIATIONS'][vv][2]), raw=True)
+        for var in self.settings['VARIATIONS'].keys():
+            min_step = self.settings['VARIATIONS'][var][0]
+            min_val  = self.settings['VARIATIONS'][var][1]
+            max_val  = self.settings['VARIATIONS'][var][2]
+            df_test.loc[:, var] = df[var].rolling(self.settings['WINDOW']).apply(lambda var_check: self.no_persistence_check(var_check, min_step, min_val, max_val), raw=True)
         df_test = df_test.fillna(False)
         return df_test.all(axis=1)
 
@@ -110,14 +113,14 @@ class InternalCheck():
     #    b = array[1]
     #    return return ((not np.isnan(a)) & (not np.isnan(b))) or ( np.isnan(a) & ((b==0) | (np.isnan(b))))
 
-    def range_check(self, array: np.array, a: np.array, b: np.array) -> bool:
+    def range_check(self, vars_check: np.array, min_vals: np.array, max_vals: np.array) -> bool:
         """This function returns True if element are in the range"""
-        return (array>=a) & (array<=b)
+        return (vars_check>=min_vals) & (vars_check<=max_vals)
 
     #def no_step_check(self, array: np.array, step: float) -> bool:
     #    """This function returns True if there are no step in a 2-d array"""
     #    return (np.abs(array[1]-array[0])<=step)
 
-    def no_persistence_check(self, array: np.array, var: float, a: float, b: float) -> bool:
+    def no_persistence_check(self, var_check: np.array, min_step: float, min_val: float, max_val: float) -> bool:
         """This function returns True if elements are not constants"""
-        return not ((np.count_nonzero((array>=a)&(array<=b))==array.shape[0]) & (np.count_nonzero(np.abs(np.ediff1d(array))<var)==(array.shape[0]-1)))
+        return not ((np.count_nonzero((var_check>=min_val)&(var_check<=max_val))==var_check.shape[0]) & (np.count_nonzero(np.abs(np.ediff1d(var_check))<min_step)==(var_check.shape[0]-1)))
